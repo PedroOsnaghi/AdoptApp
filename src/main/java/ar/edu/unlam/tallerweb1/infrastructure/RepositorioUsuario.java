e19460c799eb2aa3a65761265c857c6c328a53f5
LJVazquez <ljvazquez00@gmail.com>
2023-05-08 20:39:05 -0300

    Crea RepositorioUsuario con el decorador @Service comentado

---
+++ src/main/java/ar/edu/unlam/tallerweb1/infrastructure/RepositorioUsuario.java
@@ -0,0 +1,23 @@
+package ar.edu.unlam.tallerweb1.infrastructure;
+
+import ar.edu.unlam.tallerweb1.domain.usuarios.Usuario;
+import org.springframework.stereotype.Service;
+
+//@Service
+public class RepositorioUsuario {
+
+    //TODO
+    public Usuario guardar(Usuario usuario) {
+        return usuario;
+    }
+
+    ;
+
+    //TODO
+    public Usuario buscarPorEmail(String email) {
+        return new Usuario(email, "1234");
+    }
+
+    ;
+    //void modificar(Usuario usuario){};
+}
