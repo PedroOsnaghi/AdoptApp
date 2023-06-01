3405113dba95b55eec5bbe66177e29a3b065e1e2
void_oli <olicodea@gmail.com>
2023-06-01 15:29:11 -0300

    Test Publicaciones - Listados test

--- src/test/java/ar/edu/unlam/tallerweb1/domain/publicaciones/ServicioPublicacionTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/domain/publicaciones/ServicioPublicacionTest.java
@@ -2,20 +2,15 @@ package ar.edu.unlam.tallerweb1.domain.publicaciones;
 
 import ar.edu.unlam.tallerweb1.delivery.dto.PublicacionDto;
 import ar.edu.unlam.tallerweb1.domain.archivos.IServicioArchivo;
-import ar.edu.unlam.tallerweb1.infrastructure.RepositorioPublicacion;
-import ar.edu.unlam.tallerweb1.model.Mascota;
-import ar.edu.unlam.tallerweb1.model.Publicacion;
-import ar.edu.unlam.tallerweb1.model.Publicacion_favorito;
-import ar.edu.unlam.tallerweb1.model.Usuario;
+
+import ar.edu.unlam.tallerweb1.model.*;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.anyOf;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
@@ -48,6 +43,131 @@ public class ServicioPublicacionTest {
         List<Publicacion> resultado = alListarPublicaciones(idUsuario, publicaciones);
         assertThat(resultado).isEqualTo(publicaciones);
         assertThat(resultado).hasSize(2);
+    }
+
+    @Test
+    public void testListarPublicacionesConCantidadDeMensajesPorUsuarioId(){
+        Long idUsuario = 10L;
+        List<PublicacionMensajes> publicacionesConMensajes = dadoQueTengoUnaListaDePublicacionesConMensajes();
+        List<PublicacionMensajes> resultado = alListarPublicacionesConMensajes(idUsuario, publicacionesConMensajes);
+        assertThat(resultado).hasSize(2);
+    }
+
+    @Test
+    public void testListarFavoritosDeUnUsuario(){
+        Long idUsuario = 6L;
+        List<Publicacion_favorito> publicacionFavoritos = dadoQueTengoUnaListaDeFavoritos();
+        List<Publicacion_favorito> resultado = alListarFavoritos(idUsuario, publicacionFavoritos);
+        assertThat(resultado).hasSize(3);
+    }
+
+    @Test
+    public void testListarPublicacionesDisponibles(){
+        List<Publicacion> publicacionesDisponibles = dadoQueTengoUnaListaDePublicacionesDisponibles();
+        List<Publicacion> resultado = alListarPublicacionesDisponibles(publicacionesDisponibles);
+        assertThat(resultado).hasSize(4);
+    }
+
+    private List<Publicacion> alListarPublicacionesDisponibles(List<Publicacion> publicacionesDisponibles) {
+        when(this.repositorioPublicacion.listarPublicaciones("disponible")).thenReturn(publicacionesDisponibles);
+        return this.servicioPublicacion.listarPublicacionesDisponibles();
+    }
+
+    private List<Publicacion> dadoQueTengoUnaListaDePublicacionesDisponibles() {
+        List<Publicacion> publicaciones = new ArrayList<>();
+
+        Publicacion publicacion = new Publicacion();
+        publicacion.setId(2L);
+        publicacion.setEstado("disponible");
+
+        Publicacion publicacion2 = new Publicacion();
+        publicacion2.setId(3L);
+        publicacion2.setEstado("disponible");
+
+        Publicacion publicacion3 = new Publicacion();
+        publicacion3.setId(6L);
+        publicacion3.setEstado("disponible");
+
+        Publicacion publicacion4 = new Publicacion();
+        publicacion4.setId(7L);
+        publicacion4.setEstado("disponible");
+
+        publicaciones.add(publicacion);
+        publicaciones.add(publicacion2);
+        publicaciones.add(publicacion3);
+        publicaciones.add(publicacion4);
+
+        return publicaciones;
+    }
+
+    private List<Publicacion_favorito> alListarFavoritos(Long idUsuario, List<Publicacion_favorito> publicacionFavoritos) {
+        when(this.repositorioPublicacion.ListarFavoritosDeUsuario(anyLong())).thenReturn(publicacionFavoritos);
+        return this.servicioPublicacion.listarFavoritosDeUsuario(idUsuario);
+    }
+
+    private List<Publicacion_favorito> dadoQueTengoUnaListaDeFavoritos() {
+        List<Publicacion_favorito> publicacionFavoritos = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+        usuario.setId(6L);
+
+        Publicacion publicacion = new Publicacion();
+        publicacion.setId(2L);
+
+        Publicacion publicacion2 = new Publicacion();
+        publicacion2.setId(3L);
+
+        Publicacion publicacion3 = new Publicacion();
+        publicacion3.setId(6L);
+
+        Publicacion_favorito pf1 = new Publicacion_favorito(usuario, publicacion);
+        Publicacion_favorito pf2 = new Publicacion_favorito(usuario, publicacion2);
+        Publicacion_favorito pf3 = new Publicacion_favorito(usuario, publicacion3);
+
+        publicacionFavoritos.add(pf1);
+        publicacionFavoritos.add(pf2);
+        publicacionFavoritos.add(pf3);
+
+        return publicacionFavoritos;
+    }
+
+    private List<PublicacionMensajes> alListarPublicacionesConMensajes(Long idUsuario, List<PublicacionMensajes> publicacionesConMensajes) {
+        when(this.repositorioPublicacion.listarPublicacionesConMensajesPorUsuarioId(anyLong())).thenReturn(publicacionesConMensajes);
+        return this.servicioPublicacion.listarPublicacionesMensajesPorUsuarioId(idUsuario);
+    }
+
+    private List<PublicacionMensajes> dadoQueTengoUnaListaDePublicacionesConMensajes() {
+        List<PublicacionMensajes> publicacionMensajes = new ArrayList<>();
+
+        Usuario usuario = new Usuario();
+        usuario.setId(10L);
+
+        Mascota mascota1 = new Mascota();
+        mascota1.setUsuario(usuario);
+
+        Mascota mascota2 = new Mascota();
+        mascota2.setUsuario(usuario);
+
+        Publicacion publicacion = new Publicacion();
+        publicacion.setId(2L);
+        publicacion.setMascota(mascota1);
+
+        Publicacion publicacion2 = new Publicacion();
+        publicacion2.setId(2L);
+        publicacion2.setMascota(mascota2);
+
+        PublicacionMensajes publicacionMensajes1 = new PublicacionMensajes();
+        publicacionMensajes1.setPublicacion(publicacion);
+        publicacionMensajes1.setNew_messages(5L);
+
+        PublicacionMensajes publicacionMensajes2 = new PublicacionMensajes();
+        publicacionMensajes2.setPublicacion(publicacion2);
+        publicacionMensajes2.setNew_messages(10L);
+
+        publicacionMensajes.add(publicacionMensajes1);
+        publicacionMensajes.add(publicacionMensajes2);
+
+        return publicacionMensajes;
     }
 
     @Test
