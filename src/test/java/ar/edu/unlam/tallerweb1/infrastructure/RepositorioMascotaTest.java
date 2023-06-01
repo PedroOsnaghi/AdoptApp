60993744b0f1722f7d366296c4f954f3ec8b3475
arielandriel <46574963+arielandriel@users.noreply.github.com>
2023-06-01 01:35:41 -0300

    Test repo mascota

--- src/test/java/ar/edu/unlam/tallerweb1/infrastructure/RepositorioMascotaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/infrastructure/RepositorioMascotaTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -20,7 +22,6 @@ public class RepositorioMascotaTest extends SpringTest {
     private RepositorioUsuario repositorioUsuario;
 
 
-
     @Test
     @Transactional
     @Rollback
@@ -28,6 +29,15 @@ public class RepositorioMascotaTest extends SpringTest {
         Mascota mascota = dadoQueExisteUnaMascotaAGuardar();
         Mascota mascotaGuardada = alGuardarla(mascota);
         podemosObtenerSuId(mascotaGuardada);
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void alGuardar3MascotasCon2UsuariosDiferentesSoloMeDevuelveLasDelUsuarioBuscado(){
+        Usuario user = dadoQueUserTiene2Mascotas();
+        List<Mascota> mascotasDeUser = alListarLasMascotas(user);
+        podemosObtenerSoloLasMascotasdeUser(mascotasDeUser);
     }
 
     private void podemosObtenerSuId(Mascota mascotaGuardada) {
@@ -48,6 +58,76 @@ public class RepositorioMascotaTest extends SpringTest {
 
         return m;
     }
+
+    private Usuario dadoQueUserTiene2Mascotas() {
+        Usuario user = this.repositorioUsuario.guardarUsuario(new Usuario("test", "test@test","1234"));
+        Usuario user2 = this.repositorioUsuario.guardarUsuario(new Usuario("test2", "test2@test2","1234"));
+        Mascota m = new Mascota();
+        m.setNombre("Lulu");
+        m.setUsuario(user);
+
+        Mascota m2 = new Mascota();
+        m2.setNombre("Lulu2");
+        m2.setUsuario(user);
+
+        Mascota m3 = new Mascota();
+        m3.setNombre("Lulu3");
+        m3.setUsuario(user2);
+        this.repositorioMascota.guardar(m);
+        this.repositorioMascota.guardar(m2);
+        this.repositorioMascota.guardar(m3);
+        return user;
+    }
+
+    private List<Mascota> alListarLasMascotas(Usuario user) {
+        return this.repositorioMascota.listarMascotaPorUsuario(user);
+    }
+
+    private void podemosObtenerSoloLasMascotasdeUser(List<Mascota> mascotasSinPublicar) {
+        assertThat(mascotasSinPublicar).hasSize(2);
+    }
+/*
+    @Test
+    @Transactional
+    @Rollback
+    public void alGuardar3MascotasMeDevuelveUnaListaDeLasQueNoEstanPublicadas(){
+         Usuario user = dadoQueUnUsuarioTiene3MascotasGuardadasSinPublicar();
+        List<Mascota> mascotasSinPublicar = alListarlas(user);
+        podemosObtenerUnaListaDeLasMismas(mascotasSinPublicar);
+    }
+
+    private Usuario dadoQueUnUsuarioTiene3MascotasGuardadasSinPublicar() {
+        Usuario user = this.repositorioUsuario.guardarUsuario(new Usuario("test", "test@test","1234"));
+
+        Mascota m = new Mascota();
+        m.setNombre("Lulu");
+        m.setUsuario(user);
+
+        Mascota m2 = new Mascota();
+        m2.setNombre("Lulu2");
+        m2.setUsuario(user);
+
+        Mascota m3 = new Mascota();
+        m3.setNombre("Lulu3");
+        m3.setUsuario(user);
+        this.repositorioMascota.guardar(m);
+        this.repositorioMascota.guardar(m2);
+        this.repositorioMascota.guardar(m3);
+        return user;
+    }
+
+    private List<Mascota> alListarlas(Usuario user) {
+        return this.repositorioMascota.listarMascotasaPublicar(user);
+
+
+    }
+
+    private void podemosObtenerUnaListaDeLasMismas(List<Mascota> mascotasSinPublicar) {
+        assertThat(mascotasSinPublicar).hasSize(3);
+    }
+*/
+
+
 
 
 }
