5b3c245e1642e56dbcbbb79d27b58705b2121aee
void_oli <olicodea@gmail.com>
2023-06-14 12:49:04 -0300

    feat solicitudes
    - se agrega import de List en usuario

--- src/main/java/ar/edu/unlam/tallerweb1/model/Usuario.java
+++ src/main/java/ar/edu/unlam/tallerweb1/model/Usuario.java
@@ -4,8 +4,10 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.sql.Timestamp;
+import java.util.List;
 
 
 @Entity
