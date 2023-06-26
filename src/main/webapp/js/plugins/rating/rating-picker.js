a74959058a35a5893914e4743d17aad68ed77de2
Pedro Osnaghi <osnaghi.prog@gmail.com>
2023-06-26 19:17:53 -0300

    VISTA SOLICITUD-ADOPTANTE

---
+++ src/main/webapp/js/plugins/rating/rating-picker.js
@@ -0,0 +1,13 @@
+
+    
+
+$.ratePicker("#rating-1", {
+    rate : function (stars){
+        alert('Sample 3\'s Rate is ' + stars);
+    },
+    starSize: "25px",
+    starPadding: "5px 8px",
+    counterStyle: "color: #000;font-size:25px",
+    counterClass: "counter-star"
+});
+
