28d72df35a14243c77067b9c01413e95111cea98
void_oli <olicodea@gmail.com>
2023-07-06 15:29:27 -0300

    test controlador chat
    - refresh test

--- src/test/java/ar/edu/unlam/tallerweb1/delivery/ControladorChatTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/delivery/ControladorChatTest.java
@@ -3,8 +3,10 @@ package ar.edu.unlam.tallerweb1.delivery;
 import ar.edu.unlam.tallerweb1.domain.auth.IServicioAuth;
 import ar.edu.unlam.tallerweb1.domain.chat.IServicioChat;
 import ar.edu.unlam.tallerweb1.domain.publicaciones.IServicioPublicacion;
+import ar.edu.unlam.tallerweb1.model.ChatMensaje;
 import ar.edu.unlam.tallerweb1.model.Solicitud;
 import ar.edu.unlam.tallerweb1.model.Usuario;
+import org.dom4j.rule.Mode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -12,9 +14,12 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyObject;
@@ -41,14 +46,32 @@ public class ControladorChatTest {
     {
         Solicitud solicitud = dadoQueExisteUnMensajeParaSerEnviado();
         ResponseEntity<Timestamp> responseEntity = alEnviarlo(solicitud);
-          obtenemosLaRespuestaCorrecta(responseEntity);
+        obtenemosLaRespuestaCorrecta(responseEntity);
 
     }
 
     @Test
     public void alrefrescarLaListaDeMensajeMeDevuelveLaVistaCorrespondiente()
     {
+        Solicitud solicitud = dadoQueExisteUnaSolicitud();
+        ModelAndView vista = AlRefrescar(solicitud);
+        obtengoLaVistaCorrecta(vista);
+    }
 
+    private void obtengoLaVistaCorrecta(ModelAndView vista) {
+        assertThat(vista.getViewName()).isEqualTo("partials/chat-list");
+    }
+
+    private ModelAndView AlRefrescar(Solicitud solicitud) {
+        when(this.servicioAuth.getUsuarioAutenticado()).thenReturn(new Usuario());
+        when(this.serviciochat.listarMensajesDeSolicitud(anyString())).thenReturn(new ArrayList<>());
+        return this.controladorChat.refresh(solicitud.getCodigo());
+    }
+
+    private Solicitud dadoQueExisteUnaSolicitud() {
+        Solicitud solicitud = new Solicitud();
+        solicitud.setCodigo("1234");
+        return solicitud;
     }
 
     private Solicitud dadoQueExisteUnMensajeParaSerEnviado() {
